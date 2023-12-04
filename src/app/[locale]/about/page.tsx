6fd6dd897c0769e41383e471d7ed8ee7b953ff29
sayad-mallllek <58302539+sayad-mallllek@users.noreply.github.com>
2023-12-04 20:00:53 +0200

    Initialize i18n-2

---
+++ src/app/[locale]/about/page.tsx
@@ -0,0 +1,20 @@
+import { BaseTemplate } from '@/templates/BaseTemplate';
+
+export default function About() {
+  return (
+    <BaseTemplate>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+        recusandae quidem. Quaerat molestiae blanditiis doloremque possimus
+        labore voluptatibus distinctio recusandae autem esse explicabo molestias
+        officia placeat, accusamus aut saepe.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+        recusandae quidem. Quaerat molestiae blanditiis doloremque possimus
+        labore voluptatibus distinctio recusandae autem esse explicabo molestias
+        officia placeat, accusamus aut saepe.
+      </p>
+    </BaseTemplate>
+  );
+}
